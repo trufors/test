43bb391f3c312a6dc3e4d829112a392b7cd2fb20
trufors <yungbatya@icloud.com>
2022-11-28 15:28:19 +0300

    fix vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,19 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import { store } from './store';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ChakraProvider>
         <Provider store={store}>
           <App />
         </Provider>
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
